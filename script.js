08da96bb08d59034878abb4b9eaf168d3f9c4234
Omanshi123 <147927726+Omanshi123@users.noreply.github.com>
2024-07-28 11:34:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "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";
+const apiKey = "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";
 
 const maxImages = 4; // Number of images to generate for each prompt
 let selectedImageNumber = null;
